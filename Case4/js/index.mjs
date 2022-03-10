d791866c65a8eb74b30553cfb4d59031fe0f68c3
DanBaDo <byte4life@pm.me>
2022-03-10 08:10:19 +0100

    Adds case 4

---
+++ Case4/js/index.mjs
@@ -0,0 +1,6 @@
+function toggleShowCompletedHandler (event) {
+    const completedElements = document.querySelectorAll(".completed");
+    for ( let item of completedElements ) {
+        item.classList.toggle("hidden");
+    }
+}
